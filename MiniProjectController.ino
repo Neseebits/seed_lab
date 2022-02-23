b34f4c0a5de65f7020fe90c8332e49311b25e82f
Kjuneau <57681808+Kjuneau@users.noreply.github.com>
2022-02-22 17:18:58 -0700

    Update MiniProjectController.ino

--- MiniProjectController.ino
+++ MiniProjectController.ino
@@ -1,6 +1,7 @@
 // This code implements a proportional-integral controller
 
 #include <Encoder.h>
+#define tol 0.01
 
 Encoder wheel(2,3);
 
@@ -9,8 +10,9 @@ int input_pin = 0;
 float ratio = 1;
 
 // Variables Kp and Ki allow for adjustment of PI control
-float Kp = 0.45;
-float Ki = 13.04;
+float Kp = 1;
+float Ki = 5;
+
 
 // Values for position and integral
 float current_position = 0;
@@ -31,7 +33,9 @@ float pi = 3.1415;
 
 void setup() {
   Serial.begin(9600);
-  
+  pinMode(7,OUTPUT);
+  pinMode(4,OUTPUT);
+  digitalWrite(4,HIGH);
 }
 
 void loop() {
@@ -39,27 +43,51 @@ void loop() {
 
   // read current position
   current_position = (float)(wheel.read()) * ((2*pi)/3200);
+  if (current_position > pi) {
+    current_position -= 2*pi;
+  } else if (current_position < -pi) {
+    current_position += 2*pi;
+  }
   // read requested position
-  requested_position = analogRead(input_pin) * ratio;
-  
+  //requested_position = analogRead(input_pin) * ratio;
+  requested_position = 1.57;
+
   // calculate error
-  e = requested_position - current_position
+  e = requested_position - current_position;
   
   // calculate I
-  I += e * period;
+  if (abs(e) < tol) {
+    I = 0;
+  } else {
+    I += e * ((float)period / 1000);
+  }
 
   // set output
   u = Kp * e + Ki * I;
+
+  if (u > 0) {
+    digitalWrite(7,HIGH);
+  } else {
+    digitalWrite(7,LOW);
+    u *= -1;
+  }
   if (u > 7.5) {
     u = 7.5;
   }
-  if (u < -7.5) {
-    u = -7.5;
-  }
   u = (255/7.5) * u;
   analogWrite(9,u);
+
+  Serial.print(current_position);
+  Serial.print("\t");
+  Serial.print(requested_position);
+  Serial.print("\t");
+  Serial.print(e);
+  Serial.print("\t");
+  Serial.print(I);
+  Serial.print("\t");
+  Serial.println(u);
   
-  while (millis < time_now + period) {
+  while (millis() < time_now + period) {
     // :)
   }
 }
